11ae88f2279cd2e4a9ccd184fc210a81489f41fd
gershon <gershon@cio.gov.il>
2018-05-06 16:36:46 +0300

    update

---
+++ test/test_helper.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/users_test');
+mongoose.connection
+.once('open',() => {
+    console.log('Good to go');
+})
+.on('error',(error) => {
+    console.warn('Error : ',error);
+});
